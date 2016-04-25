22f3cc5553f537e45746f221a171d3e914729198
bhoff <bruce.hoff@sagebase.org>
2016-04-25 09:31:08 -0700

    initial commit

---
+++ R/tappingCountStatistic.R
@@ -0,0 +1,15 @@
+# This is the function to compute the tapping count statistic for the mPower tapping test
+#
+# Input is the json from Bridge Exporter, converted into an R
+# object by RJSONIO.
+#
+# Output is ??????
+# 
+# Author: bhoff
+###############################################################################
+
+
+
+tappingCountStatistic<-function() {
+	stop("Not yet implemented.")
+}
---
+++ tests/testthat/testCountStatistic.R
@@ -0,0 +1,12 @@
+# This is the set of tests for the count statistic feature
+# 
+# Author: bhoff
+###############################################################################
+
+library(testthat)
+
+context("testCountStatistic")
+
+# TODO
+expect_equal(TRUE, TRUE)
+
